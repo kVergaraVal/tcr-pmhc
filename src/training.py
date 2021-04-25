80b1f01c29b5f3ae54299f6f35016f8eb91ac1f2
kVergaraVal <67003515+kVergaraVal@users.noreply.github.com>
2021-04-25 06:06:17 -0400

    Update training.py

--- src/training.py
+++ src/training.py
@@ -1,8 +1,13 @@
-import glob
-
+#!pip install -q sklearn
+!pip install PyDrive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sc
 from sklearn import metrics
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.metrics import roc_curve, confusion_matrix
@@ -10,15 +15,20 @@ import torch
 import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
 import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
 import torch.nn.functional as F  # All functions that don't have any parameters
-from sklearn.metrics import accuracy_score
+from torch.utils.data import DataLoader, ConcatDataset
+from torchvision import transforms
+from sklearn.model_selection import KFold
+from sklearn.metrics import matthews_corrcoef
 
 ###############################
 ###    Load data            ###
 ###############################
 
+
 data_list = []
 target_list = []
 
+import glob
 for fp in glob.glob("data/train/*input.npz"):
     data = np.load(fp)["arr_0"]
     targets = np.load(fp.replace("input", "labels"))["arr_0"]
@@ -26,140 +36,205 @@ for fp in glob.glob("data/train/*input.npz"):
     data_list.append(data)
     target_list.append(targets)
 
-# Note:
-# Choose your own training and val set based on data_list and target_list
-# Here using the last partition as val set
+dataset_raw = np.concatenate(data_list)
+dataset_ros = dataset_raw[:,:,20:27]
 
-X_train = np.concatenate(data_list[ :-1])
-y_train = np.concatenate(target_list[:-1])
-nsamples, nx, ny = X_train.shape
+#min-max scaling
+#for i in range(0,len(dataset[0,0,:])):
+#  min = np.min(dataset[:,:,i])
+#  max = np.max(dataset[:,:,i])
+#  dataset[:,:,i] = (dataset[:,:,i]-min)/(max-min)
+target = np.concatenate(target_list)
+nsamples, nx, ny = dataset_ros.shape
 print("Training set shape:", nsamples,nx,ny)
 
-X_val = np.concatenate(data_list[-1: ])
-y_val = np.concatenate(target_list[-1: ])
-nsamples, nx, ny = X_val.shape
-print("val set shape:", nsamples,nx,ny)
+p_pos = len(target[target == 1])/len(target)*100
+print("Percent positive samples in dataset:", p_pos)
 
-p_neg = len(y_train[y_train == 1])/len(y_train)*100
-print("Percent positive samples in train:", p_neg)
 
-p_pos = len(y_val[y_val == 1])/len(y_val)*100
-print("Percent positive samples in val:", p_pos)
+# Fourier Transform of per-residue Rosetta scoring terms
+
+MHC = dataset_ros[:,0:180,:]
+peptide = dataset_ros[:,180:192,:]
+TCR = dataset_ros[:,192:421,:]
+
+len_MHC = len(MHC[0,:,0])
+len_peptide = len(peptide[0,:,0])
+len_TCR = len(TCR[0,:,0])
+
+num_attr = len(dataset_ros[0,0,:])
+num_samp = len(dataset_ros[:,0,0])
+
+zeropad_MHC = np.zeros((num_samp,256-len_MHC,num_attr))
+zeropad_peptide = np.zeros((num_samp,16-len_peptide,num_attr))
+zeropad_TCR = np.zeros((num_samp,256-len_TCR,num_attr))
+
+print(np.shape(MHC))
+print(np.shape(zeropad_MHC))
+
+MHC_padded = np.concatenate((MHC,zeropad_MHC),axis=1)
+peptide_padded = np.concatenate((peptide,zeropad_peptide),axis=1)
+TCR_padded = np.concatenate((TCR,zeropad_TCR),axis=1)
+print(np.shape(MHC_padded))
+
+nyq_lim_MHC = len(MHC_padded[0,:,0])
+nyq_lim_pep = len(peptide_padded[0,:,0])
+nyq_lim_TCR = len(TCR_padded[0,:,0])
+
+MHC_fft = np.zeros(np.shape(MHC_padded))
+peptide_fft = np.zeros(np.shape(peptide_padded))
+TCR_fft = np.zeros(np.shape(TCR_padded))
+
+for k in range(0,num_samp):
+  for j in range(0,num_attr):
+    MHC_fft_vec = np.abs(sc.fft.fft(MHC_padded[k,:,j]))
+    MHC_fft_vec = MHC_fft_vec[0:int(nyq_lim_MHC)]
+    MHC_fft_vec = [2*elem/(2*nyq_lim_MHC) for elem in MHC_fft_vec]
+    MHC_fft[k,:,j] = MHC_fft_vec
+
+    pep_fft_vec = np.abs(sc.fft.fft(peptide_padded[k,:,j]))
+    pep_fft_vec = pep_fft_vec[0:int(nyq_lim_pep)]
+    pep_fft_vec = [2*elem/(2*nyq_lim_pep) for elem in pep_fft_vec]
+    peptide_fft[k,:,j] = pep_fft_vec
+
+    TCR_fft_vec = np.abs(sc.fft.fft(TCR_padded[k,:,j]))
+    TCR_fft_vec = TCR_fft_vec[0:int(nyq_lim_TCR)]
+    TCR_fft_vec = [2*elem/(2*nyq_lim_TCR) for elem in TCR_fft_vec]
+    TCR_fft[k,:,j] = TCR_fft_vec
+
+
+dataset_fft = np.concatenate((MHC_fft,peptide_fft,TCR_fft),axis=1)
 
 # make the data set into one dataset that can go into dataloader
-train_ds = []
-for i in range(len(X_train)):
-    train_ds.append([np.transpose(X_train[i]), y_train[i]])
+dataset_ds = []
+for i in range(len(dataset_fft)):
+    dataset_ds.append([np.transpose(dataset_fft[i]), target[i]])
 
-val_ds = []
-for i in range(len(X_val)):
-    val_ds.append([np.transpose(X_val[i]), y_val[i]])
+print(np.shape(dataset_fft))
 
-bat_size = 64
-print("\nNOTE:\nSetting batch-size to", bat_size)
-train_ldr = torch.utils.data.DataLoader(train_ds,batch_size=bat_size, shuffle=True)
-val_ldr = torch.utils.data.DataLoader(val_ds,batch_size=bat_size, shuffle=True)
-
-
+dataset_ds_raw = []
+for i in range(len(dataset_raw)):
+    dataset_ds_raw.append([np.transpose(dataset_raw[i]), target[i]])
+    
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device (CPU/GPU):", device)
 #device = torch.device("cpu")
 
-
-
-
 ###############################
-###    Define network       ###
-###############################
-
-print("Initializing network")
-
-# Hyperparameters
-input_size = 420
-num_classes = 1
-learning_rate = 0.01
-
-# Initialize network
-from model import Net
-net = Net(num_classes=num_classes).to(device)
-
-# Loss and optimizer
-criterion = nn.BCELoss()
-optimizer = optim.SGD(net.parameters(), lr=learning_rate)
-
-
-
-###############################
-###         TRAIN           ###
+###     TRAIN WITH CV       ###
 ###############################
 
 print("Training")
 
-num_epochs = 5
+# Configuration options
+k_folds = 4
+num_epochs = 30
+bat_size = 64
+  
+# For fold results
+#results = {}
+  
+# Set fixed random number seed
+torch.manual_seed(42)
 
-train_acc, train_loss = [], []
-valid_acc, valid_loss = [], []
-losses = []
-val_losses = []
-
-for epoch in range(num_epochs):
-    cur_loss = 0
-    val_loss = 0
+# Define the K-fold Cross Validator
+kfold = KFold(n_splits=k_folds, shuffle=True)
     
-    net.train()
-    train_preds, train_targs = [], [] 
-    for batch_idx, (data, target) in enumerate(train_ldr):
-        X_batch =  data.float().detach().requires_grad_(True)
-        target_batch = torch.tensor(np.array(target), dtype = torch.float).unsqueeze(1)
-        
-        optimizer.zero_grad()
-        output = net(X_batch)
-        
-        batch_loss = criterion(output, target_batch)
-        batch_loss.backward()
-        optimizer.step()
-        
-        preds = np.round(output.detach().cpu())
-        train_targs += list(np.array(target_batch.cpu()))
-        train_preds += list(preds.data.numpy().flatten())
-        cur_loss += batch_loss.detach()
+# Start print
+print('--------------------------------')
 
-    losses.append(cur_loss / len(train_ldr.dataset))
-        
+
+# K-fold Cross Validation model evaluation
+val_loss_mean = []
+val_MCC_mean = []
+for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset_ds)):
+  net.apply(reset_weights)
+  train_acc, train_loss = [], []
+  valid_acc, valid_loss = [], []
+  losses = []
+  val_losses = []
+  best_MCC = 0
+  # Print
+  print(f'FOLD {fold}')
+  print('--------------------------------')
     
-    net.eval()
-    ### Evaluate validation
-    val_preds, val_targs = [], []
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(val_ldr): ###
-            x_batch_val = data.float().detach()
-            y_batch_val = target.float().detach().unsqueeze(1)
-            
-            output = net(x_batch_val)
-            
-            val_batch_loss = criterion(output, y_batch_val)
-            
-            preds = np.round(output.detach())
-            val_preds += list(preds.data.numpy().flatten()) 
-            val_targs += list(np.array(y_batch_val))
-            val_loss += val_batch_loss.detach()
-            
-        val_losses.append(val_loss / len(val_ldr.dataset))
-        print("\nEpoch:", epoch+1)
-        
-        train_acc_cur = accuracy_score(train_targs, train_preds)  
-        valid_acc_cur = accuracy_score(val_targs, val_preds) 
+  # Sample elements randomly from a given list of ids, no replacement.
+  train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+  test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
+    
+  # Define data loaders for training and testing data in this fold
+  train_ldr = torch.utils.data.DataLoader(
+                    dataset_ds, 
+                    batch_size=bat_size, sampler=train_subsampler)
+  val_ldr = torch.utils.data.DataLoader(
+                    dataset_ds,
+                    batch_size=bat_size, sampler=test_subsampler)
 
-        train_acc.append(train_acc_cur)
-        valid_acc.append(valid_acc_cur)
+  for epoch in range(num_epochs):
+      cur_loss = 0
+      val_loss = 0
+    
+      net.train()
+      train_preds, train_targs = [], [] 
+      for batch_idx, (data, target) in enumerate(train_ldr):
+          X_batch =  data.float().detach().requires_grad_(True)
+          target_batch = torch.tensor(np.array(target), dtype = torch.float).unsqueeze(1)
         
-        from sklearn.metrics import matthews_corrcoef
-        print("Training loss:", losses[-1].item(), "Validation loss:", val_losses[-1].item(), end = "\n")
-        print("MCC Train:", matthews_corrcoef(train_targs, train_preds), "MCC val:", matthews_corrcoef(val_targs, val_preds))
+          optimizer.zero_grad()
+          output = net(X_batch)
         
-print('\nFinished Training ...')
+          batch_loss = criterion(output, target_batch)
+          batch_loss.backward()
+          optimizer.step()
+        
+          preds = np.round(output.detach().cpu())
+          train_targs += list(np.array(target_batch.cpu()))
+          train_preds += list(preds.data.numpy().flatten())
+          cur_loss += batch_loss.detach()
 
-# Write model to disk for use in predict.py
-print("Saving model to src/model.pt")
-torch.save(net.state_dict(), "src/model.pt")
+      losses.append(cur_loss / len(train_ldr.dataset))        
+    
+      net.eval()
+      ### Evaluate validation
+      val_preds, val_targs = [], []
+      with torch.no_grad():
+          for batch_idx, (data, target) in enumerate(val_ldr): ###
+              x_batch_val = data.float().detach()
+              y_batch_val = target.float().detach().unsqueeze(1)
+            
+              output = net(x_batch_val)
+            
+              val_batch_loss = criterion(output, y_batch_val)
+            
+              preds = np.round(output.detach())
+              val_preds += list(preds.data.numpy().flatten()) 
+              val_targs += list(np.array(y_batch_val))
+              val_loss += val_batch_loss.detach()
+            
+          val_losses.append(val_loss / len(val_ldr.dataset))
+          print("\nEpoch:", epoch+1)
+        
+          train_acc_cur = accuracy_score(train_targs, train_preds)  
+          valid_acc_cur = accuracy_score(val_targs, val_preds) 
+
+          train_acc.append(train_acc_cur)
+          valid_acc.append(valid_acc_cur)
+
+          MCC_val = matthews_corrcoef(val_targs, val_preds)
+          print("Training loss:", losses[-1].item(), "Validation loss:", val_losses[-1].item(), end = "\n")
+          print("MCC Train:", matthews_corrcoef(train_targs, train_preds), "MCC val:", MCC_val)
+      
+      cur_MCC = MCC_val
+      if cur_MCC > best_MCC:
+        #best_model = copy.deepcopy(model)  # Will work
+        best_MCC = cur_MCC
+        torch.save(net.state_dict(), 'src/model.pt')
+  
+  break
+  val_loss_mean += [val_losses[-1].item()]
+  val_MCC_mean += [MCC_val]
+print('--------------------')
+print('Mean val loss: ', np.mean(val_loss_mean))
+print('Mean val MCC: ', np.mean(val_MCC_mean))   
+
