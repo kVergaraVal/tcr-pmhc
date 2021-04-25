db249c00431df41bc749e7b3cf4d76c14aef8407
kVergaraVal <67003515+kVergaraVal@users.noreply.github.com>
2021-04-25 06:12:05 -0400

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,13 @@
-import argparse
-import glob, os, tempfile, zipfile
-
+#!pip install -q sklearn
+!pip install PyDrive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sc
 from sklearn import metrics
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.metrics import roc_curve, confusion_matrix
@@ -10,76 +15,123 @@ import torch
 import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
 import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
 import torch.nn.functional as F  # All functions that don't have any parameters
-from sklearn.metrics import accuracy_score
-
-# Needed to predict
-from model import Net
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--input-zip')
-args = parser.parse_args()
-print(args)
-
-# Load files
-filenames = []
-dfs = []
-with tempfile.TemporaryDirectory() as tmpdir:
-    with zipfile.ZipFile(args.input_zip) as zip:
-        files = [file for file in zip.infolist() if file.filename.endswith(".npz")]
-        for file in files:
-            zip.extract(file, tmpdir)
-            
-        # Load npz files
-        data_list = []
-
-        for fp in glob.glob(tmpdir + "/*input.npz"):
-            data = np.load(fp)["arr_0"]
-
-            data_list.append(data)
-         
-X_test = np.concatenate(data_list[:])
-nsamples, nx, ny = X_test.shape
-print("test set shape:", nsamples,nx,ny)
-
-test_ds = []
-for i in range(len(X_test)):
-    test_ds.append([np.transpose(X_test[i])])
-
-bat_size = 64
-print("\nNOTE:\nSetting batch-size to", bat_size)
-test_ldr = torch.utils.data.DataLoader(test_ds,batch_size=bat_size, shuffle=False)
+from torch.utils.data import DataLoader, ConcatDataset
+from torchvision import transforms
+from sklearn.model_selection import KFold
+from sklearn.metrics import matthews_corrcoef
 
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device (CPU/GPU):", device)
 #device = torch.device("cpu")
 
-def predict(net, test_ldr):
-    net.eval()
-    test_preds = []
-    with torch.no_grad():
-        for batch_idx, data in enumerate(test_ldr): ###
-            x_batch_val = data[0].float().detach()
+def prediction(in_path,model_param,out_path):
 
-            output = net(x_batch_val)
-            preds = np.round(output.detach())
-            test_preds += list(preds.data.numpy().flatten()) 
-        
-    return(test_preds)
+  data_list = []
+  target_list = []
 
+  import glob
+  for fp in glob.glob(in_path):
+    data = np.load(fp)["arr_0"]
+    targets = np.load(fp.replace("input", "labels"))["arr_0"]
     
+    data_list.append(data)
+    target_list.append(targets)
 
-# import trained model
-model = Net(num_classes = 1)
-model.load_state_dict(torch.load("src/model.pt"))
-model.eval()
+  X_values = np.concatenate(data_list[:])
+  y_values = np.concatenate(target_list[:])
 
-y_pred = predict(model, test_ldr)
+  X_ros = X_values[:,:,20:27]
 
-# Write y_true, y_pred to disk
-outname = "predictions.csv"
-print("\nSaving TEST set y_pred to", outname)
-df_performance = pd.DataFrame({"ix": range(len(y_pred)), "prediction": y_pred},)
-df_performance.to_csv(outname, index=False)
+  # Fourier Transform of per-residue Rosetta scoring terms
 
-print(open(outname, 'r').read())
+  MHC = X_ros[:,0:180,:]
+  peptide = X_ros[:,180:192,:]
+  TCR = X_ros[:,192:421,:]
+
+  len_MHC = len(MHC[0,:,0])
+  len_peptide = len(peptide[0,:,0])
+  len_TCR = len(TCR[0,:,0])
+
+  num_attr = len(X_ros[0,0,:])
+  num_samp = len(X_ros[:,0,0])
+
+  zeropad_MHC = np.zeros((num_samp,256-len_MHC,num_attr))
+  zeropad_peptide = np.zeros((num_samp,16-len_peptide,num_attr))
+  zeropad_TCR = np.zeros((num_samp,256-len_TCR,num_attr))
+
+  MHC_padded = np.concatenate((MHC,zeropad_MHC),axis=1)
+  peptide_padded = np.concatenate((peptide,zeropad_peptide),axis=1)
+  TCR_padded = np.concatenate((TCR,zeropad_TCR),axis=1)
+
+  nyq_lim_MHC = len(MHC_padded[0,:,0])
+  nyq_lim_pep = len(peptide_padded[0,:,0])
+  nyq_lim_TCR = len(TCR_padded[0,:,0])
+
+  MHC_fft = np.zeros(np.shape(MHC_padded))
+  peptide_fft = np.zeros(np.shape(peptide_padded))
+  TCR_fft = np.zeros(np.shape(TCR_padded))
+
+  for k in range(0,num_samp):
+    for j in range(0,num_attr):
+      MHC_fft_vec = np.abs(sc.fft.fft(MHC_padded[k,:,j]))
+      MHC_fft_vec = MHC_fft_vec[0:int(nyq_lim_MHC)]
+      MHC_fft_vec = [2*elem/(2*nyq_lim_MHC) for elem in MHC_fft_vec]
+      MHC_fft[k,:,j] = MHC_fft_vec
+
+      pep_fft_vec = np.abs(sc.fft.fft(peptide_padded[k,:,j]))
+      pep_fft_vec = pep_fft_vec[0:int(nyq_lim_pep)]
+      pep_fft_vec = [2*elem/(2*nyq_lim_pep) for elem in pep_fft_vec]
+      peptide_fft[k,:,j] = pep_fft_vec
+
+      TCR_fft_vec = np.abs(sc.fft.fft(TCR_padded[k,:,j]))
+      TCR_fft_vec = TCR_fft_vec[0:int(nyq_lim_TCR)]
+      TCR_fft_vec = [2*elem/(2*nyq_lim_TCR) for elem in TCR_fft_vec]
+      TCR_fft[k,:,j] = TCR_fft_vec
+
+
+  dataset_fft = np.concatenate((MHC_fft,peptide_fft,TCR_fft),axis=1)
+
+  dataset_ds = []
+  for i in range(len(dataset_fft)):
+    dataset_ds.append([np.transpose(dataset_fft[i]), y_values[i]])
+
+  # load model
+
+  net.load_state_dict(torch.load(model_param))
+
+  # model evaluate
+################################################### AQUI TOI PEGAO #########################################################
+  Outputs = []
+  for k in range(0,len(dataset_ds)):
+    input = np.reshape(dataset_ds[k][0],(1,7,528))
+    input = torch.FloatTensor(input)
+
+    output = net(input)
+    output = output.detach().numpy()
+    output2 = output.tolist()
+    Outputs += output2[0]
+    bin_Outputs = [int(round(i,0)) for i in Outputs]
+
+  # output file
+  rows = [i for i in range(0,len(bin_Outputs))]
+  output_df = pd.DataFrame(bin_Outputs, index=rows, columns=['prediction'])
+  outfilename = out_path + 'predictions.csv'
+  outfile = open(outfilename, 'wb')
+  output_df.to_csv(outfilename, sep = ',', encoding = 'utf-8')
+  outfile.close()
+
+  targs = y_values.tolist()
+  acc_final = accuracy_score(targs, bin_Outputs)  
+  MCC_final = matthews_corrcoef(targs, bin_Outputs)
+  #print("Confusion matrix final:", confusion_matrix(targs, bin_Outputs), sep = "\n")
+
+  return acc_final, MCC_final
+
+
+in_path = "data/train/*input.npz"
+model_param = "scr/model.pt"
+out_path = "predictions.csv"
+acc_final, MCC_final = prediction(in_path, model_param, out_path)
+print('test accuracy: ', acc_final)
+print('test MCC: ', MCC_final)
